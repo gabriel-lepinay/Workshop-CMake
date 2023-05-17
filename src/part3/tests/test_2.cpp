5e8c7f7935501273af66dc2ae07987cdcce47eca
Gaby <gabriel.lepinay@epitech.eu>
2023-05-17 14:25:21 +0200

    []

---
+++ src/part3/tests/test_2.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** workshop-cmake
+** File description:
+** test_2.cpp
+*/
+
+#include <criterion/criterion.h>
+
+Test(dummytest, caramba)
+{
+    cr_assert(1 == 1);
+}
