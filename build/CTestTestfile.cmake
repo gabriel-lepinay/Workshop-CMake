51d05b2168f8c03c061ed387e0d18b373ef0bb34
Gaby <gabriel.lepinay@epitech.eu>
2023-05-17 14:30:39 +0200

    löschen: alte Dateien

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gxby/Github/Workshop-CMake/build
-# Build directory: /home/gxby/Github/Workshop-CMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "./testrun")
-set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/home/gxby/Github/Workshop-CMake/build/CMakeLists.txt;30;add_test;/home/gxby/Github/Workshop-CMake/build/CMakeLists.txt;0;")
